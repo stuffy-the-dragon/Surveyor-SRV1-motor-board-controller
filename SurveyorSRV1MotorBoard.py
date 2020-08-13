391c3b3c9f6ee2141addfd30cf3d7337a7b2ee4f
Alex Lucouw <lucouw@gmail.com>
2020-08-13 11:11:00 +0200

    fix(docstring): Fix docstring locations

--- SurveyorSRV1MotorBoard.py
+++ SurveyorSRV1MotorBoard.py
@@ -1,43 +1,42 @@
 from gpiozero import LED, PhaseEnableMotor
 
-""" A class that controls the motor board on the Surveyor SRV-1 tracked robot """
 class SurveyorSRV1MotorBoard:
-    """ Constuctor """
+""" A class that controls the motor board on the Surveyor SRV-1 tracked robot """
     def __init__(self):
         # A factor that can be adjusted to fine tune motor speed
-        self.__left_speed = 1.0 
-        self.__right_speed = 1.0
+        self._left_speed = 1.0 
+        self._right_speed = 1.0
 
         # The actual motor objects
-        self.__left_motor = PhaseEnableMotor(26, 20)
-        self.__right_motor = PhaseEnableMotor(16, 21)
+        self._left_motor = PhaseEnableMotor(26, 20)
+        self._right_motor = PhaseEnableMotor(16, 21)
 
         # The laser objects
-        self.__left_laser = LED(13)
-        self.__right_laser = LED(12)
+        self._left_laser = LED(13)
+        self._right_laser = LED(12)
 
-    """ Turn on both lasers """
     def lasersOn(self):
-        self.__left_laser.on()
-        self.__right_laser.on()
+    """ Turn on both lasers """
+        self._left_laser.on()
+        self._right_laser.on()
 
-    """ Turn off both lasers """
     def lasersOff(self):
-        self.__left_laser.off()
-        self.__right_laser.off()
+    """ Turn off both lasers """
+        self._left_laser.off()
+        self._right_laser.off()
 
+    @property
+    def motorsSpeedFactors(self):
     """ Get the motor speed factors 
         Output: {'left': float, 'right': float}
     """
-    @property
-    def motorsSpeedFactors(self):
-        return {'left': self.__left_speed, 'right': self.__right_speed}
+        return {'left': self._left_speed, 'right': self._right_speed}
 
+    @motorsSpeedFactors.setter
+    def motorsSpeedFactors(self, left = 1.0, right = 1.0):
     """ Tune the motor speed factors
         Input: Keyword parameters => left = 1.0 and right = 1.0
     """
-    @motorsSpeedFactors.setter
-    def motorsSpeedFactors(self, left = 1.0, right = 1.0):
         try:
             assert left <= 1.0 and left >= 0.0
             assert right <= 1.0 and right >= 0.0
@@ -47,15 +46,14 @@ class SurveyorSRV1MotorBoard:
             left = 1.0
             right = 1.0
         finally:
-            self.__left_speed = left
-            self.__right_speed = right
+            self._left_speed = left
+            self._right_speed = right
 
-
+    def motorsMove(self, direction, speed = 1.0):
     """ Command the motors to move
         Input: direction = ['left', 'right', 'forward', 'backward']
                speed = float => 0.0 to 1.0
     """
-    def motorsMove(self, direction, speed = 1.0):
         try:
             assert speed <= 1.0
             assert speed >= 0.0
@@ -65,48 +63,47 @@ class SurveyorSRV1MotorBoard:
             print("Speed parameters should be between 0.0 and 1.0")
             print("Not affecting any motor changes")
 
-        left_speed_adj = speed * self.__left_speed
-        right_speed_adj = speed * self.__right_speed
+        left_speed_adj = speed * self._left_speed
+        right_speed_adj = speed * self._right_speed
 
         if direction == 'forward':
             # Backward is forward on our robot
-            self.__left_motor.backward(left_speed_adj) 
-            self.__right_motor.backward(right_speed_adj) 
+            self._left_motor.backward(left_speed_adj) 
+            self._right_motor.backward(right_speed_adj) 
         elif direction == 'backward':
             # Forward is backward on our robot
-            self.__left_motor.forward(left_speed_adj) 
-            self.__right_motor.forward(right_speed_adj) 
+            self._left_motor.forward(left_speed_adj) 
+            self._right_motor.forward(right_speed_adj) 
         elif direction == 'left':
-            self.__left_motor.stop() 
-            self.__right_motor.backward(right_speed_adj) 
+            self._left_motor.stop() 
+            self._right_motor.backward(right_speed_adj) 
         elif direction == 'right':
-            self.__left_motor.backward(left_speed_adj) 
-            self.__right_motor.stop() 
+            self._left_motor.backward(left_speed_adj) 
+            self._right_motor.stop() 
         else:
             print("This shouldn't happen... seems like a direction command slipped through")
 
-    """ Command the motors to stop """
     def motorsStop(self):
-        self.__left_motor.stop()
-        self.__right_motor.stop()
+    """ Command the motors to stop """
+        self._left_motor.stop()
+        self._right_motor.stop()
 
+    def boardStatus(self):
     """ The status of the SRV1 motor board
         Output: {'L Laser': Boolean, 'R Laser': Boolean, 'L Motor': Boolean, 'R Motor': Boolean}
     """
-    def boardStatus(self):
-        return {'L Laser': self.__left_laser.is_lit, 
-                'R Laser': self.__right_laser.is_lit,
-                'L Motor': self.__left_motor.value,
-                'R Motor': self.__right_motor.value}
+        return {'L Laser': self._left_laser.is_lit, 
+                'R Laser': self._right_laser.is_lit,
+                'L Motor': self._left_motor.value,
+                'R Motor': self._right_motor.value}
 
-
-    """ Let the __str__ method indicate the status of the board """
     def __str__(self):
+    """ Let the __str__ method indicate the status of the board """
         return str(self.boardStatus())
 
             
-""" If this module is run as stand alone, do some tests """
 if __name__ == "__main__":
+""" If this module is run as stand alone, do some tests """
     mb = SurveyorSRV1MotorBoard()
 
     mb.lasersOn()
