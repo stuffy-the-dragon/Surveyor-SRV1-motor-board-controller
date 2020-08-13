e2bad5759c5192889ba592d97ea0a1c87d7b3a81
Alex Lucouw <lucouw@gmail.com>
2020-08-13 12:07:56 +0200

    fix(style): Indentation and style problems

--- SurveyorSRV1MotorBoard.py
+++ SurveyorSRV1MotorBoard.py
@@ -1,10 +1,13 @@
 from gpiozero import LED, PhaseEnableMotor
 
+
 class SurveyorSRV1MotorBoard:
-""" A class that controls the motor board on the Surveyor SRV-1 tracked robot """
+    """ A class that controls the motor board on the Surveyor
+    SRV-1 tracked robot
+    """
     def __init__(self):
         # A factor that can be adjusted to fine tune motor speed
-        self._left_speed = 1.0 
+        self._left_speed = 1.0
         self._right_speed = 1.0
 
         # The actual motor objects
@@ -16,27 +19,27 @@ class SurveyorSRV1MotorBoard:
         self._right_laser = LED(12)
 
     def lasersOn(self):
-    """ Turn on both lasers """
+        """ Turn on both lasers """
         self._left_laser.on()
         self._right_laser.on()
 
     def lasersOff(self):
-    """ Turn off both lasers """
+        """ Turn off both lasers """
         self._left_laser.off()
         self._right_laser.off()
 
     @property
     def motorsSpeedFactors(self):
-    """ Get the motor speed factors 
+        """ Get the motor speed factors
         Output: {'left': float, 'right': float}
-    """
+        """
         return {'left': self._left_speed, 'right': self._right_speed}
 
     @motorsSpeedFactors.setter
-    def motorsSpeedFactors(self, left = 1.0, right = 1.0):
-    """ Tune the motor speed factors
+    def motorsSpeedFactors(self, left=1.0, right=1.0):
+        """ Tune the motor speed factors
         Input: Keyword parameters => left = 1.0 and right = 1.0
-    """
+        """
         try:
             assert left <= 1.0 and left >= 0.0
             assert right <= 1.0 and right >= 0.0
@@ -49,11 +52,11 @@ class SurveyorSRV1MotorBoard:
             self._left_speed = left
             self._right_speed = right
 
-    def motorsMove(self, direction, speed = 1.0):
-    """ Command the motors to move
+    def motorsMove(self, direction, speed=1.0):
+        """ Command the motors to move
         Input: direction = ['left', 'right', 'forward', 'backward']
                speed = float => 0.0 to 1.0
-    """
+        """
         try:
             assert speed <= 1.0
             assert speed >= 0.0
@@ -68,42 +71,44 @@ class SurveyorSRV1MotorBoard:
 
         if direction == 'forward':
             # Backward is forward on our robot
-            self._left_motor.backward(left_speed_adj) 
-            self._right_motor.backward(right_speed_adj) 
+            self._left_motor.backward(left_speed_adj)
+            self._right_motor.backward(right_speed_adj)
         elif direction == 'backward':
             # Forward is backward on our robot
-            self._left_motor.forward(left_speed_adj) 
-            self._right_motor.forward(right_speed_adj) 
+            self._left_motor.forward(left_speed_adj)
+            self._right_motor.forward(right_speed_adj)
         elif direction == 'left':
-            self._left_motor.stop() 
-            self._right_motor.backward(right_speed_adj) 
+            self._left_motor.stop()
+            self._right_motor.backward(right_speed_adj)
         elif direction == 'right':
-            self._left_motor.backward(left_speed_adj) 
-            self._right_motor.stop() 
+            self._left_motor.backward(left_speed_adj)
+            self._right_motor.stop()
         else:
-            print("This shouldn't happen... seems like a direction command slipped through")
+            print("This shouldn't happen... seems like a direction \
+                command slipped through")
 
     def motorsStop(self):
-    """ Command the motors to stop """
+        """ Command the motors to stop """
         self._left_motor.stop()
         self._right_motor.stop()
 
     def boardStatus(self):
-    """ The status of the SRV1 motor board
-        Output: {'L Laser': Boolean, 'R Laser': Boolean, 'L Motor': Boolean, 'R Motor': Boolean}
-    """
-        return {'L Laser': self._left_laser.is_lit, 
+        """ The status of the SRV1 motor board
+        Output: {'L Laser': Boolean, 'R Laser': Boolean, \
+            'L Motor': Boolean, 'R Motor': Boolean}
+        """
+        return {'L Laser': self._left_laser.is_lit,
                 'R Laser': self._right_laser.is_lit,
                 'L Motor': self._left_motor.value,
                 'R Motor': self._right_motor.value}
 
     def __str__(self):
-    """ Let the __str__ method indicate the status of the board """
+        """ Let the __str__ method indicate the status of the board """
         return str(self.boardStatus())
 
-            
+
 if __name__ == "__main__":
-""" If this module is run as stand alone, do some tests """
+    """ If this module is run as stand alone, do some tests """
     mb = SurveyorSRV1MotorBoard()
 
     mb.lasersOn()
@@ -131,10 +136,3 @@ if __name__ == "__main__":
     assert mb.boardStatus()['R Motor'] == 0.0
 
     print("All tests done!")
-
-
-
-
-
-
-
--- __init__.py
+++ __init__.py
@@ -1,2 +1,5 @@
 __all__ = ['SurveyorSRV1MotorBoard']
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
 from SurveyorSRV1MotorBoard import SurveyorSRV1MotorBoard
